736eb3d64cb7b984a9c538564e8447308393da23
GreatAlex <alexguerillot1@gmail.com>
2023-12-21 14:47:31 +0100

    crud d-p-t
    document - projet - tache

---
+++ src/Controller/Admin/DocumentsCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Documents;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class DocumentsCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Documents::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
