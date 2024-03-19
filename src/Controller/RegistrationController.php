ffee3c99aa15cd3949ffe70f88b00bb78cff7a02
Alex <alexguerillot1@gmail.com>
2024-03-19 08:33:52 +0100

    register form css

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -128,7 +128,7 @@ class RegistrationController extends AbstractController
             $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
 
         } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('danger', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
+            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
             return $this->redirectToRoute('app_login');
         }
 
